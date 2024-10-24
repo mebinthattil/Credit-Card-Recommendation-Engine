f4db4e74086520762d91299ae213b8568808c530
Mebin Thattil <39584840+mebinthattil@users.noreply.github.com>
2024-10-24 10:15:14 +0530

    Added regex func for percent off

--- helper.py
+++ helper.py
@@ -55,9 +55,54 @@ def reward_type(reward_statement : str) -> str:
 
     else:
         return False
+
+def regex_extract_reward_value_PERCENT_OFF(reward_statement : str, purchase_amount : int) -> int:
+    #TODO: currently only condition where value is extractable and logic is understood is 'orders above Rs.X'. Expand to more generic cases like 'first 3 orders'
+    import re
+
+    # Any numeric value followed by % or Rs followed by mandatory "off" and optional condition for the discount
+    pattern = r"(\d+)\s*(%|Rs)\s*off\s*(on\sorders\sabove\sRs\.\s*(\d+))?"
+
+    match = re.search(pattern, reward_statement)
+    if match:
+        value = match.group(1)  # Extracts the numeric value
+        unit = match.group(2)   # Extracts % or Rs
+        condition = match.group(3) if match.group(3) else None  # Extracts the condition or "No condition"
+        amount_in_condition = match.group(4) if match.group(4) else None  # Extracts the amount from the condition
+
+        def calc_abs_value(value, unit):
+            if unit == "Rs":
+                return int(value)
+            elif unit == "%": 
+                return purchase_amount * (int(value)/100)
+
+        if not condition: #no conditions just return the reward
+            return calc_abs_value(value, unit)
+        elif condition: #if condition exists check if eligible then return reward
+            if purchase_amount > int(amount_in_condition):
+                return calc_abs_value(value, unit)
+            else:
+                return 0
+        else:
+            print("error with condition valuation ; regex percent off")
+            return 0
+
+    else:
+        print("percent_off regex failed")
+        return 0
     
-def regex_extract_reward_value(reward_statement : str, reward_type : str) -> int:
-    pass
+def regex_extract_reward_value(reward_statement : str, purchase_amount : int) -> int:
+    if reward_type(reward_statement) == "percent off":
+        return regex_extract_reward_value_PERCENT_OFF(reward_statement, purchase_amount)
+
+    elif reward_type(reward_statement) == "points":
+        pass
+    elif reward_type(reward_statement) == "cashback":
+        pass
+    elif reward_type(reward_statement) == "coupons":
+        pass
+    else:
+        return False
 
 
 def reward_value(reward_statement : str) -> int:
